2dd3bc3d83986294d89af15fa0d3c7db4e8dd9b1
ianwebb-1982 <ianwebb1982@gmail.com>
2026-06-14 21:52:50 +0100

    Second Commit

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, PLAYER_RADIUS, LINE_WIDTH
 from logger import log_state
+from player import Player
+from circleshape import CircleShape
 
-pygame.init()
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+
     print(f"Starting Asteroids with pygame version {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    player = Player(SCREEN_WIDTH /2, SCREEN_HEIGHT /2)
 
     while True:
         log_state()
@@ -19,6 +27,14 @@ def main():
                 return
     
         screen.fill("black")
+
+        player.draw(screen)
+
+        pygame.display.flip()
+
+        dt = clock.tick(60) / 1000
+        #print(dt)
+
 
 
 
