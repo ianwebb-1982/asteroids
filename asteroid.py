3cd0f34127012e5fbee2a47d0996af74e1ea161d
ianwebb-1982 <ianwebb1982@gmail.com>
2026-06-17 22:32:00 +0100

    CH4 L7 - Final Commit for Boot Dev

--- asteroid.py
+++ asteroid.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
-from constants import LINE_WIDTH
+from constants import LINE_WIDTH, ASTEROID_MIN_RADIUS
 import pygame
+from logger import log_event
+import random
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -11,3 +13,24 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius <= ASTEROID_MIN_RADIUS:
+            return
+        
+        log_event("asteroid_split")
+
+        rand_angle = random.uniform(20, 50)
+        
+        new_velocity_1 = self.velocity.rotate(rand_angle)
+        new_velocity_2 = self.velocity.rotate(-rand_angle)
+        new_radius = self.radius - ASTEROID_MIN_RADIUS
+
+        new_asteroid_1 = Asteroid(self.position.x, self.position.y, new_radius)
+        new_asteroid_2 = Asteroid(self.position.x, self.position.y, new_radius)
+
+        new_asteroid_1.velocity = new_velocity_1 * 1.2
+        new_asteroid_2.velocity = new_velocity_2 * 1.2
+
--- main.py
+++ main.py
@@ -51,6 +51,13 @@ def main():
                 print("Game over!")
                 sys.exit()
 
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collides_with(asteroid):
+                    log_event("asteroid_shot")
+                    shot.kill()
+                    asteroid.split()
+
         for draw in drawable:
             draw.draw(screen)
 
